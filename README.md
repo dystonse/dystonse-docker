5c80a497d3941604c99a8f670eb7cb85e19fcd68
Lena Schimmel <lena@serioese.gmbh>
2020-03-22 23:31:08 +0100

    First deployable version.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dystonse-docker
+
+**This repository is a part of the multi-repository project `dystonse`. See the [main repository](https://github.com/lenaschimmel/dystonse) for more information.**
+
+We aim to create a docker-compose configuration which can be used to deploy the whole dystonse stack. Currently, only a _mysql_ database and a _phpmyadmin_ interface are included.
+
+After checkout, create `secrets/mysql_dystonse_password`and `secrets/mysql_root_password` and write a password into each file. Then, execute `docker-compose up -d`.
---
+++ db_init/realtime.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Erstellungszeit: 22. Mrz 2020 um 22:18
+-- Server-Version: 8.0.19
+-- PHP-Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `dystonse`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `realtime`
+--
+
+CREATE TABLE `realtime` (
+  `id` int NOT NULL,
+  `trip_id` varchar(60) NOT NULL,
+  `stop_id` varchar(80) NOT NULL,
+  `route_id` varchar(80) NOT NULL,
+  `stop_sequence` tinyint NOT NULL,
+  `time_of_recording` timestamp NOT NULL,
+  `time_arrival_schedule` timestamp NULL DEFAULT NULL,
+  `time_arrival_estimate` timestamp NULL DEFAULT NULL,
+  `time_departure_schedule` timestamp NULL DEFAULT NULL,
+  `time_departure_estimate` timestamp NULL DEFAULT NULL,
+  `mode` tinyint NOT NULL,
+  `delay_arrival` int DEFAULT NULL,
+  `delay_departure` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `realtime`
+--
+ALTER TABLE `realtime`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `realtime`
+--
+ALTER TABLE `realtime`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
