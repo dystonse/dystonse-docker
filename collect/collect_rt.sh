d16562312f7a57750b54dd0300d773f23cd0c8be
Lena Schimmel <mail@lenaschimmel.de>
2020-03-30 22:59:18 +0200

    Add PING_URL and download into temporary location

--- collect/collect_rt.sh
+++ collect/collect_rt.sh
@@ -1,13 +1,17 @@
 set -e
 DATE=`date +"%Y-%m-%dT%H:%M:%S%:z"`
+DOWNLOAD_DIR=/files/$GTFS_DATA_SOURCE_ID/download
 COLLECT_DIR=/files/$GTFS_DATA_SOURCE_ID/rt
+mkdir -p $DOWNLOAD_DIR
 mkdir -p $COLLECT_DIR
-FILENAME_PB=$COLLECT_DIR/gtfs-rt-$DATE.pb
-FILENAME_ZIP=$COLLECT_DIR/gtfs-rt-$DATE.zip
+FILENAME_PB=$DOWNLOAD_DIR/gtfs-rt-$DATE.pb
+FILENAME_ZIP=$DOWNLOAD_DIR/gtfs-rt-$DATE.zip
+FILENAME_ZIP_FINAL=$COLLECT_DIR/gtfs-rt-$DATE.zip
 
 curl $GTFS_RT_URL -L -o $FILENAME_PB \
          --silent --show-error --retry-connrefused \
          --retry 20 --retry-max-time 100
 zip -j $FILENAME_ZIP $FILENAME_PB
+mv $FILENAME_ZIP $FILENAME_ZIP_FINAL
 rm $FILENAME_PB
 curl -s $PING_RT_URL
--- collect/collect_schedule.sh
+++ collect/collect_schedule.sh
@@ -1,11 +1,15 @@
 set -e
+DOWNLOAD_DIR=/files/$GTFS_DATA_SOURCE_ID/download
 COLLECT_DIR=/files/$GTFS_DATA_SOURCE_ID/schedule
 DATE=`date +"%Y-%m-%d"`
+mkdir -p $DOWNLOAD_DIR
 mkdir -p $COLLECT_DIR
-FILENAME_ZIP=$COLLECT_DIR/gtfs-schedule-$DATE.zip
+FILENAME_ZIP=$DOWNLOAD_DIR/gtfs-schedule-$DATE.zip
+FILENAME_ZIP_FINAL=$COLLECT_DIR/gtfs-schedule-$DATE.zip
 
 curl -s $PING_SCHEDULE_URL/start
 curl $GTFS_SCHEDULE_URL -L -o $FILENAME_ZIP \
          --silent --show-error --retry-connrefused \
          --retry 20 --retry-max-time 100
+mv $FILENAME_ZIP $FILENAME_ZIP_FINAL
 curl -s $PING_SCHEDULE_URL
