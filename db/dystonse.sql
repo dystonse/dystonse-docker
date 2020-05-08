94dbb56b310636ca1f30b2016aed540d8656a1c8
Lena Schimmel <mail@lenaschimmel.de>
2020-05-09 00:35:34 +0200

    Changed table structure for sql init and set build branch for the importer.
    Co-authored-by: Kirstin Rohwer <mail@metakiki.net>

--- db/dystonse.sql
+++ db/dystonse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Erstellungszeit: 22. Mrz 2020 um 22:44
+-- Erstellungszeit: 07. Mai 2020 um 18:36
 -- Server-Version: 8.0.19
 -- PHP-Version: 7.4.4
 
@@ -29,26 +29,28 @@ GRANT USAGE ON *.* TO `dystonse`@`%`;
 
 GRANT ALL PRIVILEGES ON `dystonse`.* TO `dystonse`@`%`;
 -- --------------------------------------------------------
+--
+-- Datenbank: `dystonse`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `realtime`
 --
 
 CREATE TABLE `realtime` (
-  `id` int NOT NULL,
-  `trip_id` varchar(60) NOT NULL,
-  `stop_id` varchar(80) NOT NULL,
-  `route_id` varchar(80) NOT NULL,
+  `source` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `route_id` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `route_variant` bigint UNSIGNED NOT NULL,
+  `trip_id` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `date` date NOT NULL,
   `stop_sequence` tinyint NOT NULL,
+  `stop_id` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `time_of_recording` timestamp NOT NULL,
-  `time_arrival_schedule` timestamp NULL DEFAULT NULL,
-  `time_arrival_estimate` timestamp NULL DEFAULT NULL,
-  `time_departure_schedule` timestamp NULL DEFAULT NULL,
-  `time_departure_estimate` timestamp NULL DEFAULT NULL,
-  `mode` tinyint NOT NULL,
   `delay_arrival` int DEFAULT NULL,
   `delay_departure` int DEFAULT NULL,
-  `source` varchar(60) NOT NULL
+  `schedule_file_name` varchar(254) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -59,19 +61,5 @@ CREATE TABLE `realtime` (
 -- Indizes für die Tabelle `realtime`
 --
 ALTER TABLE `realtime`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `realtime`
---
-ALTER TABLE `realtime`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  ADD PRIMARY KEY (`source`(5),`route_id`(10),`route_variant`,`trip_id`(10),`date`,`stop_sequence`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
